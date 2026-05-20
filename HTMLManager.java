28f34ff4e789dd2ff3fed882a949d1c5f10a246a
hashirawais162-droid <hashirawais162@gmail.com>
2026-05-20 11:23:05 -0700

    Update HTMLManager.java

--- HTMLManager.java
+++ HTMLManager.java
@@ -1,6 +1,19 @@
 import java.util.*;
 
 public class HTMLManager {
-  private Queue<HTMLTag> tags;
-  
+    private Queue<HTMLTag> tags;
+
+    public HTMLManager(Queue<HTMLTag> html) {
+        if (html == null) {
+            throw new IllegalArgumentException("Queue cannot be null");
+        }
+
+        tags = new LinkedList<>();
+
+        for (HTMLTag tag : html) {
+            tags.add(tag);
+        }
+    }
+
+   
 }
